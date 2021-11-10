8ea0ceab92cdb3abde4eed62ad6d2116582f401d
Fan-Yu Kuan <allenkuan@shiraishi.home>
2021-11-10 21:13:00 +0800

    Install zsh-autosuggestions

--- install.sh
+++ install.sh
@@ -86,7 +86,11 @@ git config --unset --global user.email
 #
 # Install oh-my-zsh
 #
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) "
+#
+# Install zsh-autosuggestions
+#
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 #
 # Clone oh-my-tmux
 #
